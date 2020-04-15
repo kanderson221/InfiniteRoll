a5e05738598f37d468cc7288a17a820a20636507
Kyle Anderson <kanderson221@student.cvtc.edu>
2020-04-15 09:21:38 -0500

    start of the Charicter js file

---
+++ infiniteRoll_capstone(MasterStyle)/js/charicter.js
@@ -0,0 +1,17 @@
+var name = prompt("What is your charicter's name?");
+
+ while (name === "") {
+		
+  fName = prompt("Reminder: Please enter your first name:");    
+				
+}
+
+var race = prompt("What is your charicter's race?");
+
+while (name === "") {
+	
+	var race = prompt("What is your charicter's race?");
+	
+}
+
+
