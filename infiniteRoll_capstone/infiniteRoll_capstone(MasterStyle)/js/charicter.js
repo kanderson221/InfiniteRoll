287f820f0751a056b6dd4846f9fa5842dd35da08
Kyle Anderson <kanderson221@student.cvtc.edu>
2020-04-20 08:48:41 -0500

    Added my js file in

---
+++ infiniteRoll_capstone/infiniteRoll_capstone(MasterStyle)/js/charicter.js
@@ -0,0 +1,27 @@
+var name = prompt("What is your charicter's name?");
+
+ while (name === "") {
+		
+  fName = prompt("Reminder: Please enter your first name:");    
+				
+}
+
+var race = prompt("What is your charicter's race?");
+
+while (name === "") {
+	
+	var race = prompt("What is your charicter's race?");
+	
+}
+
+var strength = Math.floor(Math.random() * Math.floor(3, 6)) + 1;
+var dexterity = Math.floor(Math.random() * Math.floor(3, 6)) + 1;
+var constitution = Math.floor(Math.random() * Math.floor(3, 6)) + 1;
+var intelligence = Math.floor(Math.random() * Math.floor(3, 6)) + 1;
+var wisdom = Math.floor(Math.random() * Math.floor(3, 6)) + 1;
+var charisma = Math.floor(Math.random() * Math.floor(3, 6)) + 1;
+
+
+
+
+
