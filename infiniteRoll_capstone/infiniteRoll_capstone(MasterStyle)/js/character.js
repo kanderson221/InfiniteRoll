73f37ec665ccc029b9a4f336e8d99192ac960aef
Kyle Anderson <kanderson221@student.cvtc.edu>
2020-04-22 19:02:32 -0500

    Display stats and race affects stats

--- infiniteRoll_capstone/infiniteRoll_capstone(MasterStyle)/js/character.js
+++ infiniteRoll_capstone/infiniteRoll_capstone(MasterStyle)/js/character.js
@@ -6,20 +6,64 @@ var name = prompt("What is your character's name?");
 				
 }
 
-var race = prompt("What is your character's race?");
+var race = prompt("What is your character's race? (Dwarf, Elf, Human)");
 
-while (name === "") {
+while (race === "") {
 	
 	var race = prompt("What is your character's race?");
 	
 }
 
-var strength = Math.floor(Math.random() * Math.floor(3, 6)) + 1;
-var dexterity = Math.floor(Math.random() * Math.floor(3, 6)) + 1;
+var dwarfBonus = 0;
+var elfBonus = 0;
+var humanBonus = 0;
+
+if (race === "Dwarf" || race === "dwarf") {
+	dwarfBonus = 1;
+} else if (race === "Elf" || race === "elf") {
+	elfBonus = 1;
+} else if (humanBonus === "Human" || race === "human") {
+	humanBonus = 1;
+}
+
+var strength = Math.floor(Math.random() * Math.floor(3, 6)) + 1 + dwarfBonus;
+var dexterity = Math.floor(Math.random() * Math.floor(3, 6)) + 1 + humanBonus;
 var constitution = Math.floor(Math.random() * Math.floor(3, 6)) + 1;
 var intelligence = Math.floor(Math.random() * Math.floor(3, 6)) + 1;
-var wisdom = Math.floor(Math.random() * Math.floor(3, 6)) + 1;
+var wisdom = Math.floor(Math.random() * Math.floor(3, 6)) + 1 + elfBonus;
 var charisma = Math.floor(Math.random() * Math.floor(3, 6)) + 1;
+
+document.getElementById("strength").textContent = "Strength: " + strength;
+document.getElementById("dexterity").textContent = "Dexterity: " + dexterity;
+document.getElementById("constitution").textContent = "Constitution: " + constitution;
+document.getElementById("intelligence").textContent = "Intelligence: " + intelligence;
+document.getElementById("wisdom").textContent = "Wisdom: " + wisdom;
+document.getElementById("charisma").textContent = "Charisma: " + charisma;
+
+document.getElementById("name").textContent = "Name: " + name;
+document.getElementById("race").textContent = "Race: " + race;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
