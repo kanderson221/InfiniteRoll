4a942a5428e8206bd3c78bbf67895bd39dd58c4a
Kyle Anderson <kanderson221@student.cvtc.edu>
2020-04-27 11:28:25 -0500

    Charicter update

--- infiniteRoll_capstone/infiniteRoll_capstone(MasterStyle)/js/character.js
+++ infiniteRoll_capstone/infiniteRoll_capstone(MasterStyle)/js/character.js
@@ -2,7 +2,7 @@ var name = prompt("What is your character's name?");
 
  while (name === "") {
 		
-  name = prompt("Reminder: Please enter your name:");    
+  name = prompt("Please enter a name.");    
 				
 }
 
@@ -10,9 +10,28 @@ var race = prompt("What is your character's race? (Dwarf, Elf, Human)");
 
 while (race === "") {
 	
-	var race = prompt("What is your character's race?");
+	var race = prompt("Please enter one of these races: Dwarf, Elf, Human");
 	
 }
+
+while (race != "Dwarf" && race != "dwarf" && race != "Elf" && race != "elf" && race != "Human" && race != "human") {
+		var race = prompt("Please enter one of these races: Dwarf, Elf, Human");
+}
+
+var className = prompt("What is your character's class? (Wizard, Fighter, Paladin)");
+
+while (className === "") {
+	var className = prompt("Please enter one of these classes: Wizard, Fighter, Paladin")
+}
+
+while (className != "Wizard" && className != "wizard" && className != "Fighter" && className != "fighter" && className != "Paladin" && className != "paladin") {
+	var className = prompt("Please enter one of these classes: Wizard, Fighter, Paladin")
+}
+
+var rollOne = 0;
+var rollTwo = 0;
+var rollThree = 0;
+var rollFour = 0;
 
 var dwarfBonus = 0;
 var elfBonus = 0;
@@ -22,7 +41,7 @@ if (race === "Dwarf" || race === "dwarf") {
 	dwarfBonus = 1;
 } else if (race === "Elf" || race === "elf") {
 	elfBonus = 1;
-} else if (humanBonus === "Human" || race === "human") {
+} else if (race === "Human" || race === "human") {
 	humanBonus = 1;
 }
 
@@ -33,15 +52,36 @@ var intelligence = Math.floor(Math.random() * Math.floor(3, 6)) + 1;
 var wisdom = Math.floor(Math.random() * Math.floor(3, 6)) + 1 + elfBonus;
 var charisma = Math.floor(Math.random() * Math.floor(3, 6)) + 1;
 
-document.getElementById("strength").textContent = "Strength: " + strength;
-document.getElementById("dexterity").textContent = "Dexterity: " + dexterity;
-document.getElementById("constitution").textContent = "Constitution: " + constitution;
-document.getElementById("intelligence").textContent = "Intelligence: " + intelligence;
-document.getElementById("wisdom").textContent = "Wisdom: " + wisdom;
-document.getElementById("charisma").textContent = "Charisma: " + charisma;
+document.getElementById("strength").textContent = strength;
+document.getElementById("dexterity").textContent = dexterity;
+document.getElementById("constitution").textContent = constitution;
+document.getElementById("intelligence").textContent = intelligence;
+document.getElementById("wisdom").textContent = wisdom;
+document.getElementById("charisma").textContent = charisma;
 
-document.getElementById("name").textContent = "Name: " + name;
-document.getElementById("race").textContent = "Race: " + race;
+document.getElementById("name").textContent = name;
+document.getElementById("race").textContent = race;
+
+if (className === "Wizard" || className === "wizard") {
+	rollOne = Math.floor(Math.random() * Math.floor(6)) + 1;
+	rollTwo = Math.floor(Math.random() * Math.floor(6)) + 1;
+	rollThree = Math.floor(Math.random() * Math.floor(6)) + 1;
+	rollFour = Math.floor(Math.random() * Math.floor(6)) + 1;
+	
+	var hpTotal = rollOne + rollTwo + rollThree + rollFour;
+	
+} else if (className === "Fighter" || className === "fighter" || className === "Paladin" || className === "paladin") {
+	rollOne = Math.floor(Math.random() * Math.floor(6)) + 1;
+	rollTwo = Math.floor(Math.random() * Math.floor(6)) + 1;
+	rollThree = Math.floor(Math.random() * Math.floor(6)) + 1;
+	rollFour = Math.floor(Math.random() * Math.floor(6)) + 1;
+	
+	var hpTotal = rollOne + rollTwo + rollThree + rollFour;
+	
+} 
+
+
+document.getElementById("hp").textContent = hpTotal;
 
 
 
